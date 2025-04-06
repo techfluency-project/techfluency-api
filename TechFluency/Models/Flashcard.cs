fbddcec61ad354964676bb9a83e405f9371f094d
Lohhan <lohhang05@gmail.com>
2025-04-06 20:56:30 -0300

    updated logic of flashcard model

--- TechFluency/Models/Flashcard.cs
+++ TechFluency/Models/Flashcard.cs
@@ -22,22 +22,27 @@ namespace TechFluency.Models
 
         [BsonElement("difficulty")]
         [BsonRepresentation(BsonType.String)]
-        public EnumDifficulty Difficulty { get; set; }  
+        [BsonIgnoreIfNull]
+        public EnumDifficulty Difficulty { get; set; }
 
         [BsonElement("easeFactor")]
         [BsonRepresentation(BsonType.Double)]
-        public double EaseFactor { get; set; } 
+        public double EaseFactor { get; set; } = 2.5;
 
         [BsonElement("nextReviewDate")]
         [BsonRepresentation(BsonType.DateTime)]
-        public DateTime NextReviewDate { get; set; }  
+        public DateTime NextReviewDate { get; set; } = DateTime.UtcNow.AddDays(1);
 
         [BsonElement("lastReviewed")]
         [BsonRepresentation(BsonType.DateTime)]
-        public DateTime LastReviewed { get; set; }  
+        public DateTime LastReviewed { get; set; } = DateTime.UtcNow;
 
         [BsonElement("repetitionInterval")]
         [BsonRepresentation(BsonType.Int32)]
-        public int RepetitionInterval { get; set; } 
+        public int RepetitionInterval { get; set; } = 1;
+
+        [BsonElement("flashcardGroupId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string FlashcardGroupId { get; set; }
     }
 }
