39c1eaec6fdc74db491ba99f628fc73c5af37c1d
Artur Coelho <102534007+PopCPS@users.noreply.github.com>
2025-04-14 18:36:34 -0300

    Desativa politica CORS

--- TechFluency/Program.cs
+++ TechFluency/Program.cs
@@ -4,6 +4,18 @@ using TechFluency.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Cria o servico pra desativar o cors pro localhost:3000
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -26,6 +38,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// desativa o cors
+
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
