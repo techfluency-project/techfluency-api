4be8af266499c696585c73fa96e8636f413d96fa
Alice Pereira <alicecavalcanti24@gmail.com>
2025-04-21 23:26:01 -0300

    [Feature/authentication](feat): Add service to authenticate user credentials

---
+++ TechFluency/Services/JwtService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TechFluency.Context;
+using TechFluency.DTOs;
+using TechFluency.Repository;
+using static BCrypt.Net.BCrypt;
+
+namespace TechFluency.Services
+{
+    public class JwtService
+    {
+        private readonly UserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+        public JwtService(UserRepository userRepository, IConfiguration configuration) {
+            _userRepository = userRepository;
+            _configuration = configuration;
+        }
+
+         public async Task<LoginResponseDTO?> Authenticate(LoginRequestDTO loginDTO)
+          {
+            if (string.IsNullOrWhiteSpace(loginDTO.Username) || string.IsNullOrWhiteSpace(loginDTO.Password)) return null;
+              
+            var userAccount = await _userRepository.GetUserByUsername(loginDTO.Username);
+            var requestPassword = HashPassword(loginDTO.Password);
+            if (userAccount is null || Verify(requestPassword, userAccount.Password))
+                return null;  
+
+            var issuer = _configuration["JwtConfig:Issuer"];
+            var audience = _configuration["JwtConfig:Audience"];
+            var key = _configuration["JwtConfig:Key"];
+            var tokenValidityMins = _configuration.GetValue<int>("JwtConfig:TokenValidityMins");
+            var tokenExpiryTimeStamp = DateTime.UtcNow.AddMinutes(tokenValidityMins);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Name, loginDTO.Username)
+                }),
+                Expires = tokenExpiryTimeStamp,
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                        SecurityAlgorithms.HmacSha512Signature
+                )
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var acessToken = tokenHandler.WriteToken(securityToken);
+
+            return new LoginResponseDTO
+            {
+                AcessToken = acessToken,
+                ExpiresIn = (int)tokenExpiryTimeStamp.Subtract(DateTime.UtcNow).TotalSeconds,
+                UserName = loginDTO.Username
+            };
+         }
+      
+
+    }
+}
