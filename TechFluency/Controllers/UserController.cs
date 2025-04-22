453ae3b7d502b24d43556722716845a8b52a1b7b
Alice Pereira <alicecavalcanti24@gmail.com>
2025-04-21 23:35:34 -0300

    [Feature/authentication](feat): Add new features for user registration and login

---
+++ TechFluency/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechFluency.DTOs;
+using TechFluency.Models;
+using TechFluency.Services;
+
+namespace TechFluency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class UserController: ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly JwtService _jwtService;
+
+        public UserController(UserService userService, JwtService jwtService) {
+            _userService = userService;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("sign-up")]
+        public User UserRegistration(UserRegistrationDTO userRequest)
+        {
+           return _userService.userRegistration(userRequest);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("sign-in")]
+        public async Task<ActionResult<LoginResponseDTO>> Login(LoginRequestDTO loginRequest)
+        {
+            var result = await _jwtService.Authenticate(loginRequest);
+            if (result == null) 
+                return Unauthorized();
+
+             return result;
+        }
+
+    }
+}
