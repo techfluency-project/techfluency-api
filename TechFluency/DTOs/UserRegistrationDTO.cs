ea248abb0eff7f2dad23314ef150767da48d64e9
Alice Pereira <alicecavalcanti24@gmail.com>
2025-04-21 23:31:08 -0300

    [Feature/authentication](feat): Add method and related changes for user registration

---
+++ TechFluency/DTOs/UserRegistrationDTO.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace TechFluency.DTOs
+{
+    public class UserRegistrationDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? Birthdate { get; set; }
+
+    }
+}
---
+++ TechFluency/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TechFluency.DTOs;
+using TechFluency.Models;
+using TechFluency.Repository;
+using static BCrypt.Net.BCrypt;
+
+namespace TechFluency.Services
+{
+    public class UserService
+    {
+        public UserRepository _userRepository;
+
+       
+        public UserService(UserRepository userRepository) { 
+            _userRepository = userRepository;
+        }
+
+        public User userRegistration(UserRegistrationDTO userRequest)
+        {
+            userRequest.Password = HashPassword(userRequest.Password);
+            var user = new User
+            {
+                Username = userRequest.Username,
+                Password = userRequest.Password,
+                Name = userRequest.Name,
+                LastName = userRequest.LastName,
+                Email = userRequest.Email,
+                Gender = userRequest.Gender,
+                Birthdate = userRequest.Birthdate,
+                Phone = userRequest.Phone
+            };
+
+            _userRepository.Add(user);
+            return _userRepository.Get(user.Id);
+        } 
+    }
+}
